a85581c6d88ec071e0cd02529d688eef6c721458
joey <joey.abouharb@gmail.com>
2019-08-24 16:35:23 +1000

    implemented search and list functionality for pokemon

--- requests/pokemon/client.py
+++ requests/pokemon/client.py
@@ -1,7 +1,10 @@
 
 
 import requests
+import json
 
+
+MAX_POKEMON = 964
 class Client():
     """
     client service to interact with the api
@@ -9,23 +12,37 @@ class Client():
     _URL = "https://pokeapi.co/api/v2"
 
     def __init__(self):
-        self._setup()
-
-
-    def _setup(self):
-        r = requests.get(
-            f'{self._URL}/pokemon'
-        )
-        if r.status_code == 200:
-            self.pokemon = r.json()['results']
-        else:
-            self.pokemon = []
-
-
-    def get_pokemon(self):
         pass
 
+    def get_pokemon(self, limit=20, offset=0):
+        params = f'?limit={limit}&offset={offset}'
+        r = requests.get(
+            f'{self._URL}/pokemon/{params}'
+        )
+        if r.status_code == 200:
+            self.pokemon = json.dumps(r.json()['results'], indent=2)
+        else:
+            print('Connection Error. ')
 
+    def find_by_id(self, id):
+        r = requests.get(
+            f'{self._URL}/pokemon/{id}'
+        )
+        if r.status_code == 200:
+            self.pokemon = json.dumps(r.json(), indent=4)
+        else:
+            exit('connection error')
+    
+    def find_by_name(self, name):
+        r = requests.get(
+            f'{self._URL}/pokemon/{name}'
+        )
+        if r.status_code == 200:
+            self.pokemon = json.dumps(r.json(), indent=4)
+        elif r.status_code == 404:
+            exit('pokemon not found')
+        else:
+            exit('connection error!')
 if __name__ == '__main__':
     c = Client()
     c.pokemon
--- requests/pokemon/main.py
+++ requests/pokemon/main.py
@@ -2,26 +2,74 @@
 """
 CLI for our pokemon app
 """
-from client import Client
+import argparse
+from client import Client, MAX_POKEMON
 
-def main():
+
+def main(args):
     """
     mock endpoint that handles interaction with api
     """
+    c = Client()
+
+    if args.subcommand == 'search':
+        if args.id:
+            find_pokemon_by_id(c, args.id)
+        elif args.name:
+            find_pokemon_by_name(c, args.name)
+    elif args.subcommand == 'list':
+        retrieve_all_pokemon(c, args.limit)
+
+
+def find_pokemon_by_id(c, id):
+    c.find_by_id(id)
+    print(c.pokemon)
+
+
+def find_pokemon_by_name(c, name):
+    c.find_by_name(name)
+    print(c.pokemon)
+
+
+def retrieve_all_pokemon(c, limit):
     exit_flag = False
+    offset = 0
+
     while exit_flag ==  False:
-        command = input('Enter a command:  ')
-        if command == "hello":
-            print("Hello, World!")
-        elif command == "pokemon":
-            c = Client()
-            total = len(c.pokemon)
-            if total:
-                print(f'There are {total} pokemon')
-            else:
-                print('Connection Error. ')
-        elif command == "exit":
+        c.get_pokemon(limit, offset)
+        print(c.pokemon)
+        offset += limit
+
+        if offset > MAX_POKEMON:
             exit_flag = True
+        input('Press Enter to Retrieve More: ')
+
+
 
 if __name__ == '__main__':
-    main()
+    PARSER = argparse.ArgumentParser(
+        prog='Pokedex',
+        usage='Program that retrieves pokemon data, or search a known pokemon by id, or name',
+        description='Pokedex App that gives you access to all known pokemon. gotta catch them all!'
+    )
+
+    SUBCOMMAND = PARSER.add_subparsers(
+        title='Subcommands',
+        description='execute a subcommand to retrieve data from the pokedex',
+        prog='API Requests', dest='subcommand'
+    )
+    LIST = SUBCOMMAND.add_parser(name='list')
+    LIST.add_argument(
+        '--limit', type=int, help='limit search results per request', default=0
+    )
+    POKEMON = SUBCOMMAND.add_parser(name='search')
+    POKEMON.add_argument(
+        '--id', help='search pokemon via id', type=int, default=20
+    )
+    POKEMON.add_argument(
+        '--name', help='search pokemon via name', type=str, default=''
+    )
+    ARGS = PARSER.parse_args()
+    input(f'{ARGS}')
+    main(ARGS)
+
