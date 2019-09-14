5b0a2c293f0f5f4991d2c7fbca915a2f0bf5f982
joeyabouharb <38250119+joeyabouharb@users.noreply.github.com>
2019-09-14 21:42:59 +1000

    added json file to store addresses

--- week4/minitorrent/minitorrent/services/broadcast.py
+++ week4/minitorrent/minitorrent/services/broadcast.py
@@ -18,16 +18,12 @@ broadcaster.settimeout(1)
 # bind socket to broadcast at localhost, port 44444
 broadcaster.bind(("", 44444))
 
-reciever = socket(
-    AF_INET, SOCK_STREAM
-)
-reciever.bind(('', 37202))
-reciever.listen(1)
+
 output = b"is broadcasting!"
 broadcasting = True
 while broadcasting:
     # broadcast to clients listening on port 37020
     broadcaster.sendto(output, ('<broadcast>', 37020))
     print("sending IP!")
-    broadcasting = False
+    break
 
--- week4/minitorrent/minitorrent/services/listener.py
+++ week4/minitorrent/minitorrent/services/listener.py
@@ -3,7 +3,10 @@ from socket import (
     socket, AF_INET, SOCK_DGRAM, IPPROTO_UDP,
     SOL_SOCKET, SO_BROADCAST, SOCK_STREAM
 )
-
+import time
+import json
+import datetime
+addresses = {}
 listener = socket(
     AF_INET, SOCK_DGRAM, IPPROTO_UDP
 )
@@ -16,10 +19,16 @@ respond = socket(
     AF_INET, SOCK_STREAM
 )
 
-listening = True
-while listening:
-    data, addr = listener.recvfrom(16)
-    location = ":".join(f'{value}' for value in addr)
-    print(f"{location} {data.decode('utf8')}")
-    
-    listening = False
+
+def generate_recieved_addresses():
+    listening = True
+    while listening:
+        data, addr = listener.recvfrom(16)
+        location = ":".join(f'{value}' for value in addr)
+        print(f"{location} {data.decode('utf8')}")
+        yield addr
+
+for addr in generate_recieved_addresses():
+    addresses[addr[0]] = f'{datetime.datetime.today()}'
+    with open('addresses.json', 'w+') as file:
+        json.dump(addresses, file, indent=2)
